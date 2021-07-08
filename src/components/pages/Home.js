eecf027ae85d574c5895233ef0f8a0b7ddde02dc
paucabral <josepaucabral@gmail.com>
2021-07-09 04:26:19 +0800

    Added homepage information.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,8 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <React.Fragment>
-      <div style={{ height: "100vh"}}>
-        <h3>home</h3>
+      <div className="container" style={{ height: "100vh"}}>
+        <h3 className="text-white">Welcome to Pau Cabral's PokéDex!</h3>
+        <br/>
+        <p className="text-white">This is a PokeDex site that allow users to view Pokémon and their corresponding information fetched from <b>PokéAPI</b>.</p>
+        <br/>
+        <p className="text-white">NOTE: This home page, along with other features is still working in progress. You may click on the 'ENTRIES' tab to view the list of Pokémon and view their corresponding information page.</p>
       </div>
     </React.Fragment>
   )

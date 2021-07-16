f0cf62ea636ff8a2edb49b20ce5d75ce32c96ec9
paucabral <josepaucabral@gmail.com>
2021-07-17 01:14:52 +0800

    Added flask backend server to serve model.

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+from reverseproxy import proxyRequest
+from classifier import classifyImage
+
+MODE = os.getenv('FLASK_ENV')
+DEV_SERVER_URL = os.getenv('FRONTEND')
+
 app = Flask(__name__)
+
+# Ignore static folder in development mode.
+if MODE == "development":
+    app = Flask(__name__, static_folder=None)
 
 
 @app.route('/')
-def index():
-    return "Hello from FLASK"
+@app.route('/<path:path>')
+def index(path=''):
+    if MODE == 'development':
+        return proxyRequest(DEV_SERVER_URL, path)
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run()
+
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    if (request.files['image']):
+        file = request.files['image']
+
+        result = classifyImage(file)
+        print('Model classification: ' + result[0])
+        return result
---
+++ backend/classifier.py
@@ -0,0 +1,46 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Specify model.
+MODEL_PATH = os.getenv('MODEL_PATH')
+print(MODEL_PATH)
+model = keras.models.load_model(MODEL_PATH)
+
+
+def getPrediction(img_bytes, model):
+    img_height = 200
+    img_width = 200
+
+    img = keras.preprocessing.image.load_img(
+        img_bytes, target_size=(img_height, img_width)
+    )
+
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+    predictions = model.predict(img_array)
+
+    return predictions
+
+
+def classifyImage(file):
+    # Returns a probability scores matrix
+    preds = getPrediction(file, model)
+
+    score = tf.nn.softmax(preds[0])
+
+    eval_score = (np.max(score)/(np.max(score) + np.min(score))) * 100
+    best = np.argmax(score)
+
+    # Class names arranged based on the arrangement of classification paths.
+    pokemon_list = ['abra', 'aerodactyl', 'alakazam', 'arbok', 'arcanine', 'articuno', 'beedrill', 'bellsprout', 'blastoise', 'bulbasaur', 'butterfree', 'caterpie', 'chansey', 'charizard', 'charmander', 'charmeleon', 'clefable', 'clefairy', 'cloyster', 'cubone', 'dewgong', 'diglett', 'ditto', 'dodrio', 'doduo', 'dragonair', 'dragonite', 'dratini', 'drowzee', 'dugtrio', 'eevee', 'ekans', 'electabuzz', 'electrode', 'exeggcute', 'exeggutor', 'farfetchd', 'fearow', 'flareon', 'gastly', 'gengar', 'geodude', 'gloom', 'golbat', 'goldeen', 'golduck', 'golem', 'graveler', 'grimer', 'growlithe', 'gyarados', 'haunter', 'hitmonchan', 'hitmonlee', 'horsea', 'hypno', 'ivysaur', 'jigglypuff', 'jolteon', 'jynx', 'kabuto', 'kabutops', 'kadabra', 'kakuna', 'kangaskhan', 'kingler', 'koffing', 'krabby', 'lapras', 'lickitung', 'machamp', 'machoke', 'machop', 'magikarp',
+                    'magmar', 'magnemite', 'magneton', 'mankey', 'marowak', 'meowth', 'metapod', 'mew', 'mewtwo', 'moltres', 'mr-mime', 'muk', 'nidoking', 'nidoqueen', 'nidoran-f', 'nidoran-m', 'nidorina', 'nidorino', 'ninetales', 'oddish', 'omanyte', 'omastar', 'onix', 'paras', 'parasect', 'persian', 'pidgeot', 'pidgeotto', 'pidgey', 'pikachu', 'pinsir', 'poliwag', 'poliwhirl', 'poliwrath', 'ponyta', 'porygon', 'primeape', 'psyduck', 'raichu', 'rapidash', 'raticate', 'rattata', 'rhydon', 'rhyhorn', 'sandshrew', 'sandslash', 'scyther', 'seadra', 'seaking', 'seel', 'shellder', 'slowbro', 'slowpoke', 'snorlax', 'spearow', 'squirtle', 'starmie', 'staryu', 'tangela', 'tauros', 'tentacool', 'tentacruel', 'vaporeon', 'venomoth', 'venonat', 'venusaur', 'victreebel', 'vileplume', 'voltorb', 'vulpix', 'wartortle', 'weedle', 'weepinbell', 'weezing', 'wigglytuff', 'zapdos', 'zubat']
+
+    result = [pokemon_list[best], eval_score]
+
+    return result
